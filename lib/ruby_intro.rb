014b0ee3f1e77e2bb70f64c8ec4ab3b4de49a69f
Ruiyou Ma <rma@sandiego.edu>
2018-09-14 03:42:50 +0000

    hw1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,117 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+    sums = 0
+    arr.each do |x|
+        sums += x
+    end
+    return sums
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.empty?
+        return 0
+    end
+    arr1 = arr.sort{ |a, b| b <=> a }
+    if arr.length == 1
+        return arr1[0]
+    end
+    return arr1[0] + arr1[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    if arr.length <= 1
+        return false
+    end
+    arr.each do |x|
+        if arr.include?(n-x)
+            if 2*x == n
+                return true unless arr.count(x) == 1
+            else
+                return true
+            end
+        end
+    end
+    return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    if s.empty?
+        return false
+    end
+    s1 = s.capitalize
+    if ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'].include?(s1[0])
+        return true
+    else
+        return false
+    end
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s.length <= 2
+        return s == "0"
+    end
+    s.each_char do |x|
+        unless ['0', '1'].include?(x)
+            return false
+        end
+    end
+    s1 = s.reverse
+    if s1[0,2] == "00"
+        return true
+    else
+        return false
+    end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    
+    def initialize(isbn, price)
+        if isbn.empty?
+            raise ArgumentError
+        end
+        if price <= 0
+            raise ArgumentError
+        end
+        @isbn, @price = isbn, price
+    end
+    
+    def isbn
+        @isbn
+    end
+    
+    def price
+        @price
+    end
+    
+    def isbn=(new_isbn)
+        @isbn = new_isbn
+    end
+    
+    def price=(new_price)
+        @price = new_price
+    end
+    
+    def price_as_string
+        s = "$" + price.to_s
+        if s.include?('.')
+            if s.length - s.index('.') == 3
+                return s
+            elsif s.length - s.index('.') == 2
+                return s + '0'
+            end
+        else
+            return s + ".00"
+        end
+    end
+    
 end
